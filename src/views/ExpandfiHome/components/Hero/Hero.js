daea570654085b15f6cd03ad84dc4d98501d1dd8
Joe Giampaoli <giampaoli.joe@gmail.com>
2021-04-16 13:49:35 -0700

    new sheet target

--- src/views/ExpandfiHome/components/Hero/Hero.js
+++ src/views/ExpandfiHome/components/Hero/Hero.js
@@ -162,7 +162,7 @@ const Hero = props => {
     // console.log(email);
   }
 
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbyBimTtxeLuTI8DOKTcYlTrIwrhQmLNe3_7AvvZy147EAbfXF75j0-1laaN5GcLnGJJ/exec'
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwU7nkBhW8xfiT86PzF7i_lDdG0_fBHiV5M6UVsHmaJTl7nX_jsORuWXPBvm0EpmT8Q/exec'
   const form = document.forms['submit-to-google-sheet']
 
 
